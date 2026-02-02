fbb3468681531c6b5e3c0642b0adbb3a2313aab3
Hoa Duc Viet <hoaducviet@Viet-MBP.local>
2026-02-02 14:24:21 +0700

    Add new package chromium for get url

--- src/fetchHtml.js
+++ src/fetchHtml.js
@@ -1,3 +1,6 @@
+const puppeteer = require("puppeteer-core");
+const chromium = require("@sparticuz/chromium-min");
+
 function toStr(x) {
   return String(x ?? "").trim();
 }
@@ -6,6 +9,45 @@ function isHttpUrl(u) {
   return /^https?:\/\//i.test(u);
 }
 
+// const viewport = {
+//   deviceScaleFactor: 1,
+//   hasTouch: false,
+//   height: 1080,
+//   isLandscape: true,
+//   isMobile: false,
+//   width: 1920,
+// };
+
+// async function getBrowser() {
+//   return puppeteer.launch({
+//     args: puppeteer.defaultArgs({ args: chromium.args, headless: "shell" }),
+//     defaultViewport: viewport,
+//     executablePath: await chromium.executablePath("../chromium"),
+//     headless: "shell",
+//   });
+// }
+
+// async function fetchHtml(url, { timeoutMs = 20000 } = {}) {
+//   url = toStr(url);
+//   if (!isHttpUrl(url)) throw new Error("url must start with http(s)://");
+
+//   try {
+//     const browser = await getBrowser();
+
+//     const page = await browser.newPage();
+
+//     await page.goto(url, { waitUntil: "load", timeout: timeoutMs });
+
+//     const html = await page.content();
+//     await browser.close();
+//     if (!html) throw new Error("empty html");
+//     return html;
+//   } catch (error) {
+//     throw new Error(`Puppeteer error: ${error.message}`);
+//   }
+// }
+
+// Dự phòng trường hợp bị lỗi
 async function fetchHtml(url, { timeoutMs = 12000 } = {}) {
   url = toStr(url);
   if (!isHttpUrl(url)) throw new Error("url must start with http(s)://");
@@ -37,3 +79,24 @@ async function fetchHtml(url, { timeoutMs = 12000 } = {}) {
 }
 
 module.exports = { fetchHtml };
+
+// await page.setRequestInterception(true);
+// page.on("request", (request) => {
+//   const url = request.url();
+//   if (
+//     url.includes("advertising") ||
+//     url.includes("ad") ||
+//     url.includes(".jpg") ||
+//     url.includes(".png") ||
+//     url.includes(".gif") || // Chặn cả hình ảnh động
+//     url.includes("video") || // Chặn video
+//     url.includes(".css") || // Chặn CSS nếu không cần thiết
+//     url.includes(".js") // Chặn Javascript nếu không cần thiết
+//   ) {
+//     request.abort(); // Chặn tài nguyên không cần thiết
+//   } else {
+//     request.continue(); // Tiếp tục tải các tài nguyên cần thiết
+//   }
+// });
+
+// "https://github.com/Sparticuz/chromium/releases/download/v143.0.4/chromium-v143.0.4-pack.arm64.tar",
